86a4d457bcb60eba6dfbb06eec33eeaa322c6157
Rizwan <ansaririzwan8866@gmail.com>
2026-06-11 21:03:59 +0530

    bug fixes of voet T32

--- voetbalshirtskoning/T32/index.js
+++ voetbalshirtskoning/T32/index.js
@@ -14,6 +14,34 @@
             callback(elements) : setTimeout(() => waitForElement(waitFor, callback, minElements, isVariable, timer - frequency), frequency);
     }
 
+    function validatePrinting(root) {
+        const checkbox = root.querySelector('.option-printing input[type="checkbox"]');
+        const nameInput = root.querySelector('.option-name input');
+        const numberInput = root.querySelector('.option-rugnummer input');
+
+        if (!checkbox || !nameInput || !numberInput) {
+            return true;
+        }
+
+        if (!checkbox.checked) {
+            nameInput.setCustomValidity('');
+            return true;
+        }
+
+        const hasValue = nameInput.value.trim() || numberInput.value.trim();
+
+        if (!hasValue) {
+            nameInput.setCustomValidity(
+                'Vul minimaal een naam of een nummer in.'
+            );
+
+            return false;
+        }
+
+        nameInput.setCustomValidity('');
+        return true;
+    }
+
     function syncFields(sourceRoot, targetRoot) {
 
         sourceRoot.querySelectorAll('input, select, textarea').forEach(sourceField => {
@@ -21,138 +49,215 @@
 
             if (!name) return;
 
-            sourceField.addEventListener('input', () => {
-                const targetField = targetRoot.querySelector(`[name="${CSS.escape(name)}"]`);
-                if (!targetField) return;
+            ['input', 'change'].forEach(eventName => {
+                sourceField.addEventListener(eventName, () => {
+                    const targetField = targetRoot.querySelector(`[name="${CSS.escape(name)}"]`);
+                    if (!targetField) return;
 
-                // CHECKBOX
-                if (sourceField.type === 'checkbox') {
-                    targetField.checked = sourceField.checked;
+                    // CHECKBOX
+                    if (sourceField.type === 'checkbox') {
+                        targetField.checked = sourceField.checked;
+
+                        targetField.dispatchEvent(
+                            new Event('change', {
+                                bubbles: true
+                            })
+                        );
+
+                        // PRINTING OPTION
+                        if (sourceField.closest('.option-printing')) {
+                            if (!sourceField.checked) {
+                                ['.option-name input', '.option-rugnummer input'].forEach(selector => {
+                                    const sourceInput = sourceRoot.querySelector(selector);
+                                    const targetInput = targetRoot.querySelector(selector);
+
+                                    if (sourceInput) {
+                                        sourceInput.value = '';
+                                        sourceInput.dispatchEvent(
+                                            new Event('input', {
+                                                bubbles: true
+                                            })
+                                        );
+                                    }
+
+                                    if (targetInput) {
+                                        targetInput.value = '';
+                                        targetInput.dispatchEvent(
+                                            new Event('input', {
+                                                bubbles: true
+                                            })
+                                        );
+                                    }
+                                });
+                            }
+                        }
+
+                        return;
+                    }
+
+                    // RADIO
+                    if (sourceField.type === 'radio') {
+                        const radio = targetRoot.querySelector(`[name="${CSS.escape(name)}"][value="${sourceField.value}"]`);
+
+                        if (radio) {
+                            radio.checked = sourceField.checked;
+
+                            radio.dispatchEvent(
+                                new Event('change', {
+                                    bubbles: true
+                                })
+                            );
+                        }
+
+                        return;
+                    }
+
+                    // TEXT / SELECT
+                    targetField.value = sourceField.value;
+
+                    targetField.dispatchEvent(
+                        new Event('input', {
+                            bubbles: true
+                        })
+                    );
 
                     targetField.dispatchEvent(
                         new Event('change', {
                             bubbles: true
                         })
                     );
-
-                    return;
-                }
-
-                // RADIO
-                if (sourceField.type === 'radio') {
-                    const radio = targetRoot.querySelector(`[name="${CSS.escape(name)}"][value="${sourceField.value}"]`);
-
-                    if (radio) {
-                        radio.checked = sourceField.checked;
-
-                        radio.dispatchEvent(
-                            new Event('change', {
-                                bubbles: true
-                            })
-                        );
-                    }
-
-                    return;
-                }
-
-                // TEXT / SELECT
-                targetField.value = sourceField.value;
-
-                targetField.dispatchEvent(
-                    new Event('input', {
-                        bubbles: true
-                    })
-                );
-
-                targetField.dispatchEvent(
-                    new Event('change', {
-                        bubbles: true
-                    })
-                );
+                });
             });
         });
     }
 
     waitForElement("#product-options-wrapper", ([producPage]) => {
         document.querySelector('body').classList.add(testInfo.className);
-        if (!document.querySelector('.gmd-personalize-overlay')) {
-            document.body.insertAdjacentHTML('beforeend', `
-                <div class="gmd-personalize-overlay"></div>
-                <div class="gmd-personalize-popup">
-                    <div class="gmd-personalize-popup-inner">
-                        <button class="gmd-personalize-close">
-                            <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" fill="none">
-                                <path d="M7.07139 19.5L5.67139 18.1L11.2714 12.5L5.67139 6.9L7.07139 5.5L12.6714 11.1L18.2714 5.5L19.6714 6.9L14.0714 12.5L19.6714 18.1L18.2714 19.5L12.6714 13.9L7.07139 19.5Z" fill="#4B5563"/>
-                            </svg></button>
-                        <div class="gmd-personalize-content">
-                            <h2 class="gmd-title">Personaliseer je product</h2>
+        setTimeout(() => {
+            if (!document.querySelector('.gmd-personalize-overlay')) {
+                document.body.insertAdjacentHTML('beforeend', `
+                    <div class="gmd-personalize-overlay"></div>
+                    <div class="gmd-personalize-popup">
+                        <div class="gmd-personalize-popup-inner">
+                            <button class="gmd-personalize-close">
+                                <svg xmlns="http://www.w3.org/2000/svg" width="25" height="25" viewBox="0 0 25 25" fill="none">
+                                    <path d="M7.07139 19.5L5.67139 18.1L11.2714 12.5L5.67139 6.9L7.07139 5.5L12.6714 11.1L18.2714 5.5L19.6714 6.9L14.0714 12.5L19.6714 18.1L18.2714 19.5L12.6714 13.9L7.07139 19.5Z" fill="#4B5563"/>
+                                </svg></button>
+                            <div class="gmd-personalize-content">
+                                <h2 class="gmd-title">Personaliseer je product</h2>
+                            </div>
                         </div>
-                    </div>
-                </div>`
-            );
-            // setTimeout(() => {
-            document.querySelector('.gmd-personalize-content').insertAdjacentElement('beforeend', document.querySelector('#product-options-wrapper').cloneNode(true));
-            syncFields(
-                document.querySelector('.gmd-personalize-content #product-options-wrapper'),
-                document.querySelector('.product-main-info #product-options-wrapper')
-            );
-            document.querySelector('.gmd-personalize-content .product-options-wrapper').insertAdjacentElement('afterend', document.querySelector('.product-addtocart-button').cloneNode(true));
-            // }, 200)
-        }
-        const usps = document.querySelector('.product-usp-wrapper');
-        if (!document.querySelector('.gmd-printing-wrapper')) {
-            usps.insertAdjacentHTML('beforebegin', `
-                <div class="gmd-printing-wrapper">
-                    <button class="gmd-printing-btn">Bedrukking toevoegen</button>
-                </div>    
-                `)
+                    </div>`
+                );
+                // setTimeout(() => {
+                document.querySelector('.gmd-personalize-content').insertAdjacentElement('beforeend', document.querySelector('#product-options-wrapper').cloneNode(true));
+                syncFields(
+                    document.querySelector('.gmd-personalize-content #product-options-wrapper'),
+                    document.querySelector('.product-main-info #product-options-wrapper')
+                );
 
-            document.querySelector('.gmd-printing-btn').addEventListener('click', (e) => {
-                const productFieldsDesk = document.querySelectorAll(".product-main-info .form-select")
-                const productFieldsMob = document.querySelectorAll(".product-options-mobile")
-                let hasEmptyRequiredField = true;
-                if (productFieldsDesk.length) hasEmptyRequiredField = Array.from(document.querySelectorAll(".product-main-info .form-select")).some(field => field.required && !field.value);
-                if (productFieldsMob.length) hasEmptyRequiredField = document.querySelectorAll(".product-options-mobile")[0].querySelector('.field input:checked') ? false : true;
-                if (!hasEmptyRequiredField) {
-                    openPersonalizePopup();
-                } else {
-                    document.querySelector('.product-main-info .product-addtocart-button').click();
+                const popupCheckbox = document.querySelector('.gmd-personalize-popup .option-printing input[type="checkbox"]');
+
+                popupCheckbox.addEventListener('change', () => {
+
+                    document.querySelectorAll('.gmd-personalize-popup .option-name, .gmd-personalize-popup .option-rugnummer')
+                        .forEach(el => {
+                            el.classList.toggle('hidden', !popupCheckbox.checked);
+                        });
+                });
+                const popupRoot = document.querySelector('.gmd-personalize-popup #product-options-wrapper');
+
+                const nameInput = popupRoot.querySelector('.option-name input');
+                const numberInput = popupRoot.querySelector('.option-rugnummer input');
+
+                [nameInput, numberInput].forEach(input => {
+                    input?.addEventListener('input', () => {
+
+                        const hasValue = nameInput.value.trim() || numberInput.value.trim();
+                        if (hasValue) {
+                            nameInput.setCustomValidity('');
+                        }
+                    });
+                });
+                document.querySelector('.gmd-personalize-content .product-options-wrapper').insertAdjacentElement('afterend', document.querySelector('.product-addtocart-button').cloneNode(true));
+                // }, 200)
+            }
+            const usps = document.querySelector('.product-usp-wrapper');
+            if (!document.querySelector('.gmd-printing-wrapper')) {
+                usps.insertAdjacentHTML('beforebegin', `
+                    <div class="gmd-printing-wrapper">
+                        <button class="gmd-printing-btn">Bedrukking toevoegen</button>
+                    </div>    
+                    `)
+
+                document.querySelector('.gmd-printing-btn').addEventListener('click', (e) => {
+                    const productFieldsDesk = document.querySelectorAll(".product-main-info .form-select")
+                    const productFieldsMob = document.querySelectorAll(".product-options-mobile")
+                    let hasEmptyRequiredField = true;
+                    if (productFieldsDesk.length) hasEmptyRequiredField = Array.from(document.querySelectorAll(".product-main-info .form-select")).some(field => field.required && !field.value);
+                    if (productFieldsMob.length) hasEmptyRequiredField = document.querySelectorAll(".product-options-mobile")[0].querySelector('.field input:checked') ? false : true;
+                    if (!hasEmptyRequiredField) {
+                        openPersonalizePopup();
+                    } else {
+                        document.querySelector('.product-main-info .product-addtocart-button').click();
+                    }
+                })
+            }
+
+            document.querySelector('.gmd-personalize-content .product-addtocart-button').addEventListener('click', (e) => {
+                e.preventDefault();
+                const popupRoot = document.querySelector('.gmd-personalize-popup #product-options-wrapper');
+                if (!validatePrinting(popupRoot)) {
+                    popupRoot.querySelector('.option-name input').reportValidity();
+                    return;
                 }
+                document.querySelector('.product-main-info .product-addtocart-button').click();
             })
-        }
 
-        function openPersonalizePopup() {
-            document.querySelector('.gmd-personalize-overlay')?.classList.add('is-open');
-            document.querySelector('.gmd-personalize-popup')?.classList.add('is-open');
-            document.body.classList.add('gmd-overflow-hidden');
-            const inputCheckbox = document.querySelector('.gmd-personalize-popup #product-options-wrapper .option-printing input[type="checkbox"]');
-            if (!inputCheckbox.checked) {
-                setTimeout(() => {
+            function openPersonalizePopup() {
+                document.querySelector('.gmd-personalize-overlay')?.classList.add('is-open');
+                document.querySelector('.gmd-personalize-popup')?.classList.add('is-open');
+                document.body.classList.add('gmd-overflow-hidden');
+                const inputCheckbox = document.querySelector('.gmd-personalize-popup #product-options-wrapper .option-printing input[type="checkbox"]');
+                if (!inputCheckbox.checked) {
+                    setTimeout(() => {
+                        inputCheckbox.click();
+                    }, 200)
+                }
+            }
+
+            function closePersonalizePopup() {
+                document.querySelector('.gmd-personalize-overlay')?.classList.remove('is-open');
+                document.querySelector('.gmd-personalize-popup')?.classList.remove('is-open');
+                document.body.classList.remove('gmd-overflow-hidden');
+                const inputCheckbox = document.querySelector('.gmd-personalize-popup #product-options-wrapper .option-printing input[type="checkbox"]');
+                if (inputCheckbox.checked) {
                     inputCheckbox.click();
-                }, 200)
+                }
             }
-        }
 
-        function closePersonalizePopup() {
-            document.querySelector('.gmd-personalize-overlay')?.classList.remove('is-open');
-            document.querySelector('.gmd-personalize-popup')?.classList.remove('is-open');
-            document.body.classList.remove('gmd-overflow-hidden');
-            const inputCheckbox = document.querySelector('.gmd-personalize-popup #product-options-wrapper .option-printing input[type="checkbox"]');
-            if (inputCheckbox.checked) {
-                inputCheckbox.click();
-            }
-        }
+            const overlay = document.querySelector('.gmd-personalize-overlay');
+            const popup = document.querySelector('.gmd-personalize-popup');
+            const closeBtn = document.querySelector('.gmd-personalize-close');
 
-        const overlay = document.querySelector('.gmd-personalize-overlay');
-        const popup = document.querySelector('.gmd-personalize-popup');
-        const closeBtn = document.querySelector('.gmd-personalize-close');
+            closeBtn.addEventListener('click', closePersonalizePopup);
 
-        closeBtn.addEventListener('click', closePersonalizePopup);
+            overlay.addEventListener('click', closePersonalizePopup);
 
-        overlay.addEventListener('click', closePersonalizePopup);
+            popup.addEventListener('click', (e) => {
+                e.stopPropagation();
+            });
 
-        popup.addEventListener('click', (e) => {
-            e.stopPropagation();
-        });
+            window.addEventListener('pageshow', (event) => {
+                document.querySelectorAll('.option-name input, .option-rugnummer input').forEach(input => {
+                    input.value = '';
+                });
+                document.querySelectorAll('.option-printing input[type="checkbox"]').forEach(input => {
+                    if (input.checked) {
+                        input.click();
+                    }
+                });
+            });
+        }, 200)
     });
 })();
