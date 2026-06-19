c7028a1aaee9706c513bcb86f59daf9e09322723
Rizwan <ansaririzwan8866@gmail.com>
2026-06-19 21:23:20 +0530

    verpakgigant T9 done

--- verpakgigant/T9/index.js
+++ verpakgigant/T9/index.js
@@ -18,21 +18,17 @@
             error1: 'Vul je e-mailadres in',
             error2: 'Vul een geldig e-mailadres in'
         },
-
         firstName: 'Vul je voornaam in',
         lastName: 'Vul je achternaam in',
-
-        phone: 'Vul je telefoonnummer in',
-
+        phone: {
+            error1: 'Vul je telefoonnummer in',
+            error2: 'Vul een geldig telefoonnummer in'
+        },
         houseNumber: 'Vul je huisnummer in',
         address: 'Vul je adres in',
-
         streetAndNumber: 'Vul je straat en huisnummer in',
-
         place: 'Vul je woonplaats in',
-
         country: 'Selecteer een land',
-
         postalCode: {
             error1: 'Vul je postcode in',
             error2: 'Vul een geldige postcode in'
@@ -48,32 +44,27 @@
     waitForElement('.woocommerce-checkout', () => {
         document.body.classList.add(testInfo.className);
         function addRequiredPlaceholderAsterisk() {
-
             document.querySelectorAll('.form-row.validate-required input, .form-row.validate-required select').forEach(field => {
-
                 const placeholder = field.getAttribute('placeholder');
-
-                if (
-                    placeholder &&
-                    !placeholder.trim().endsWith('*')
-                ) {
-                    field.setAttribute(
-                        'placeholder',
-                        `${placeholder} *`
-                    );
+                if (placeholder && !placeholder.trim().endsWith('*')) {
+                    field.setAttribute('placeholder', `${placeholder} *`);
                 }
             });
             document.querySelectorAll('.form-row.validate-required label').forEach(field => {
-
                 const label = field;
-
-                if (
-                    label &&
-                    !label.childNodes[0].textContent.trim().includes('*')
-                ) {
+                if (label && !label.childNodes[0].textContent.trim().includes('*')) {
                     field.childNodes[0].textContent = `${label.childNodes[0].textContent} *`
                 }
             });
+
+            const companyPlaceholder = document.querySelector('#billing_company_field input')?.getAttribute('placeholder');
+            if (!companyPlaceholder?.includes(`"optioneel"`)) {
+                document.querySelector('#billing_company_field input').setAttribute('placeholder', `${companyPlaceholder} "optioneel"`)
+            }
+            const companyLabel = document.querySelector('#billing_company_field label')?.textContent;
+            if (!companyLabel?.includes(`"optioneel"`)) {
+                document.querySelector('#billing_company_field label').textContent = `${companyLabel} "optioneel"`;
+            }
         }
         addRequiredPlaceholderAsterisk();
 
@@ -121,7 +112,18 @@
                     }
                 }
 
-            } else {
+            } else if (name === 'billing_phone' || name === 'shipping_phone') {
+                if (!value) {
+                    errorEl.textContent = errorMsgs.phone.error1;
+                    valid = false;
+
+                } else if (!/^[0-9+\s()-]+$/.test(value)) {
+                    errorEl.textContent = errorMsgs.phone.error2;
+                    valid = false;
+                }
+            }
+
+            else {
                 if (!value) {
                     valid = false;
                 }
@@ -275,25 +277,26 @@
 
         validateFilledFieldsOnLoad();
 
-        // const target = document.querySelector('.woocommerce-billing-fields__field-wrapper');
-        // if (target) {
-        //     const observer = new MutationObserver(() => {
-        //         checkValidations();
-        //     });
-        //     observer.observe(target, {
-        //         childList: true,
-        //         // subtree: true,
-        //         // attributes: true,
-        //         // attributeFilter: ["class"]
-        //     })
-        // }
-
         document.addEventListener('click', function (e) {
 
             const btn = e.target.closest('#place_order');
             if (!btn) return;
 
-            const inputFields = document.querySelectorAll('.woocommerce-billing-fields__field-wrapper .validate-required input.input-text, .woocommerce-billing-fields__field-wrapper .validate-required select');
+            let selector = `
+                .woocommerce-billing-fields__field-wrapper .validate-required input.input-text,
+                .woocommerce-billing-fields__field-wrapper .validate-required select
+            `;
+
+            const shipDifferent = document.querySelector('#ship-to-different-address-checkbox');
+
+            if (shipDifferent?.checked) {
+                selector += `,
+                    .woocommerce-shipping-fields__field-wrapper .validate-required input.input-text,
+                    .woocommerce-shipping-fields__field-wrapper .validate-required select
+                `;
+            }
+
+            const inputFields = document.querySelectorAll(selector);
             let firstInvalid = null;
 
             inputFields.forEach((input) => {
@@ -306,6 +309,19 @@
             if (firstInvalid) {
                 e.preventDefault();
                 e.stopImmediatePropagation();
+
+                const billingBtn = document.querySelector('.gmd-expand-billing-address');
+                if (billingBtn && !billingBtn.classList.contains('is-open')) {
+                    billingBtn.click();
+                }
+
+                const shipDifferent = document.querySelector('#ship-to-different-address-checkbox');
+                if (shipDifferent?.checked) {
+                    const shippingBtn = document.querySelector('.gmd-expand-shipping-address');
+                    if (shippingBtn && !shippingBtn.classList.contains('is-open')) {
+                        shippingBtn.click();
+                    }
+                }
                 firstInvalid.scrollIntoView({
                     behavior: 'smooth',
                     block: 'center'
@@ -317,96 +333,210 @@
             }
         }, true);
 
+        function syncBillingToShipping() {
+
+            const fieldMap = {
+                billing_first_name: 'shipping_first_name',
+                billing_last_name: 'shipping_last_name',
+                billing_company: 'shipping_company',
+                billing_phone: 'shipping_phone'
+            };
+
+            // Live sync
+            document.addEventListener('input', (e) => {
+
+                const shippingEnabled = document.querySelector('#ship-to-different-address-checkbox')?.checked;
+
+                if (!shippingEnabled) return;
+
+                const shippingFieldName = fieldMap[e.target.name];
+
+                if (!shippingFieldName) return;
+                const shippingField = document.querySelector(`[name="${shippingFieldName}"]`);
+
+                if (shippingField) {
+                    shippingField.value = e.target.value;
+                    shippingField.dispatchEvent(
+                        new Event('change', { bubbles: true })
+                    );
+                }
+            });
+
+            document.addEventListener('change', (e) => {
+                if (e.target.id !== 'ship-to-different-address-checkbox') {
+                    return;
+                }
+
+                if (!e.target.checked) return;
+
+                Object.entries(fieldMap).forEach(
+                    ([billingName, shippingName]) => {
+                        const billingField = document.querySelector(`[name="${billingName}"]`);
+                        const shippingField = document.querySelector(`[name="${shippingName}"]`);
+
+                        if (billingField && shippingField) {
+                            shippingField.value = billingField.value;
+
+                            shippingField.dispatchEvent(
+                                new Event('change', {
+                                    bubbles: true
+                                })
+                            );
+                        }
+                    }
+                );
+            });
+        }
+
+        syncBillingToShipping();
+
         function createAddressWrappers() {
             const billingWrapper = document.querySelector('.woocommerce-billing-fields__field-wrapper');
             const shippingWrapper = document.querySelector('.woocommerce-shipping-fields__field-wrapper');
 
             if (billingWrapper && !billingWrapper.querySelector('.gmd-billing-address-fields-wrapper')) {
-                billingWrapper.insertAdjacentHTML('beforeend', '<section class="gmd-billing-address-fields-wrapper"></section>');
+                billingWrapper.insertAdjacentHTML('beforeend', `
+                    <div class="gmd-billing-address-wrapper">
+                        <div class="gmd-heading">Factuuradres</div>
+                        <div class="gmd-billing-address-fields-wrapper"></div>
+                        <a href="javascript:void(0)" class="gmd-expand-billing-address">Adres automatisch invoeren</a>
+                    </div>
+                `);
             }
 
             if (shippingWrapper && !shippingWrapper.querySelector('.gmd-shipping-address-fields-wrapper')) {
-                shippingWrapper.insertAdjacentHTML('beforeend', '<section class="gmd-shipping-address-fields-wrapper"></section>');
+                shippingWrapper.insertAdjacentHTML('beforeend', `
+                    <div class="gmd-shipping-address-wrapper">
+                        <div class="gmd-heading">Afleveradres</div>
+                        <div class="gmd-shipping-address-fields-wrapper"></div>
+                        <a href="javascript:void(0)" class="gmd-expand-shipping-address">Adres automatisch invoeren</a>
+                    </div>
+                `);
             }
+        }
+
+        function moveFieldsIntoWrapper(
+            parentSelector,
+            fieldsWrapperSelector,
+            wrapperParentSelector,
+            fields
+        ) {
+            const parent = document.querySelector(parentSelector);
+
+            if (!parent) return;
+
+            const wrapperParent = parent.querySelector(wrapperParentSelector);
+            const fieldsWrapper = parent.querySelector(fieldsWrapperSelector);
+
+            if (!wrapperParent || !fieldsWrapper) return;
+
+            while (fieldsWrapper.firstChild) {
+                parent.insertBefore(
+                    fieldsWrapper.firstChild,
+                    wrapperParent
+                );
+            }
+
+            fields.forEach(selector => {
+                const field = parent.querySelector(selector);
+                if (field) {
+                    fieldsWrapper.appendChild(field);
+                }
+            });
+
+            parent.appendChild(wrapperParent);
         }
 
         function moveAddressFields() {
-            const billingContainer = document.querySelector('.gmd-billing-address-fields-wrapper');
-            const shippingContainer = document.querySelector('.gmd-shipping-address-fields-wrapper');
 
-            const billingFields = [
-                '#billing_address_1_field',
-                '#billing_house_number_field',
-                '#billing_city_field',
-                '#billing_state_field',
-                '#billing_postcode_field',
-                '#billing_country_field'
-            ];
+            moveFieldsIntoWrapper(
+                '.woocommerce-billing-fields__field-wrapper',
+                '.gmd-billing-address-fields-wrapper',
+                '.gmd-billing-address-wrapper',
+                [
+                    '#billing_address_1_field',
+                    '#billing_city_field',
+                    '#billing_postcode_field',
+                    '#billing_country_field',
+                    '#billing_state_field'
+                ]
+            );
 
-            const shippingFields = [
-                '#shipping_address_1_field',
-                '#shipping_house_number_field',
-                '#shipping_city_field',
-                '#shipping_state_field',
-                '#shipping_postcode_field',
-                '#shipping_country_field'
-            ];
+            moveFieldsIntoWrapper(
+                '.woocommerce-shipping-fields__field-wrapper',
+                '.gmd-shipping-address-fields-wrapper',
+                '.gmd-shipping-address-wrapper',
+                [
+                    '#shipping_address_1_field',
+                    '#shipping_city_field',
+                    '#shipping_postcode_field',
+                    '#shipping_country_field',
+                    '#shipping_state_field'
+                ]
+            );
+        }
 
-            if (billingContainer && !document.querySelector('.woocommerce-billing-fields__field-wrapper .rpgaac_billing #billing_address_1_field')) {
-                billingFields.forEach(selector => {
-                    const field = document.querySelector(selector);
-                    console.log(field, 'field')
-                    if (field && field.parentElement !== billingContainer) {
-                        billingContainer.insertAdjacentElement('afterbegin', field);
-                    }
-                });
-            }
-
-            // if (shippingContainer) {
-            //     shippingFields.forEach(selector => {
-            //         const field = document.querySelector(selector);
-            //         if (field && field.parentElement !== shippingContainer) {
-            //             shippingContainer.insertAdjacentElement('afterbegin', field);
-            //         }
-            //     });
-            // }
+        function runAfterUpdate() {
+            createAddressWrappers();
+            moveAddressFields();
+            checkValidations();
+            bindValidationEvents(document.querySelector('.woocommerce-billing-fields__field-wrapper'));
+            bindValidationEvents(document.querySelector('.woocommerce-shipping-fields__field-wrapper'));
+            addRequiredPlaceholderAsterisk();
+            bindAddressExpand();
         }
 
         function observeAddressFields() {
-            const billingWrapper = document.querySelectorAll('.woocommerce-billing-fields__field-wrapper .form-row');
-            const shippingWrapper = document.querySelectorAll('.woocommerce-shipping-fields__field-wrapper .form-row');
-
-            const config = {
-                childList: true,
-                subtree: true
-            };
-
-            const billingObserver = new MutationObserver(() => {
-                if (billingWrapper.length) {
-                    moveAddressFields();
-                }
-            })
-
-            billingObserver.observe(document.querySelector('.woocommerce-billing-fields__field-wrapper'), config);
-
-            // if (shippingWrapper.length) {
-            //     new MutationObserver(callback).observe(document.querySelector('.woocommerce-shipping-fields__field-wrapper'), config);
-            // }
+            // Wait 500ms after updated_checkout so RPGAAC finishes its DOM manipulation,
+            // then reorganize with moveAddressFields which handles any resulting DOM state
+            jQuery(document.body).on('updated_checkout', function () {
+                setTimeout(runAfterUpdate, 500);
+            });
         }
 
         createAddressWrappers();
         moveAddressFields();
         observeAddressFields();
 
-        const form = document.querySelector('form.woocommerce-checkout');
-        if (form) {
-            const observer = new MutationObserver(() => {
-                addRequiredPlaceholderAsterisk();
-            })
-            observer.observe(form, {
-                childList: true,
-                subtree: true
-            })
+        function bindAddressExpand() {
+
+            document.querySelectorAll('.gmd-expand-billing-address, .gmd-expand-shipping-address').forEach(btn => {
+                if (btn.dataset.gmdBound) return;
+                btn.dataset.gmdBound = '1';
+                btn.addEventListener('click', function () {
+                    const isBilling = this.classList.contains('gmd-expand-billing-address');
+                    const fields = isBilling
+                        ? [
+                            '#billing_city_field',
+                            '#billing_country_field',
+                            '#billing_state_field'
+                        ]
+                        : [
+                            '#shipping_city_field',
+                            '#shipping_country_field',
+                            '#shipping_state_field'
+                        ];
+
+                    const showClass = isBilling
+                        ? 'gmd-show-billing'
+                        : 'gmd-show-shipping';
+
+                    const isOpen = this.classList.toggle('is-open');
+
+                    fields.forEach(selector => {
+                        document.querySelector(selector)?.classList.toggle(
+                            showClass,
+                            isOpen
+                        );
+                    });
+
+                    this.textContent = isOpen
+                        ? 'Adres handmatig invoeren'
+                        : 'Adres automatisch invoeren';
+                });
+
+            });
+
         }
     })
 })();
